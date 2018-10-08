9d98df1728f658cdd81d17c2d36c86c5edebd8c4
deadion2ttip <39771562+deadion2ttip@users.noreply.github.com>
2018-10-08 17:02:13 -0400

    stopwatch can be started.

--- stopwatch/stopwatch.cs
+++ stopwatch/stopwatch.cs
@@ -8,6 +8,36 @@ namespace stopwatch
 {
     class stopwatch
     {
-        
+        private bool _isRunning = false;
+
+        private DateTime _startTime;
+
+        private DateTime _endTime;
+
+        public TimeSpan TimeElapsed
+        {
+            get { try
+                {
+                    return _endTime - _startTime;
+                }
+                catch
+                {
+                    throw new InvalidOperationException("Must start and stop stopwatch before checking time");
+                }
+            }
+        }
+
+        public void Start()
+        {
+            if (!_isRunning)
+            {
+                this._startTime = DateTime.Now;
+                Console.WriteLine("Stopwatch started at " + _startTime);
+                _isRunning = true;
+            }
+            else
+                throw new InvalidOperationException("stopwatch cannot be started twice in a row");
+
+        }
     }
 }

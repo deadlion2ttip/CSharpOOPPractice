2e6c202c9f3bae5e396a8d6abffbc197fdfae1ef
deadion2ttip <39771562+deadion2ttip@users.noreply.github.com>
2018-10-08 17:51:27 -0400

    Initial commit StackOverFlowPostMock. Set up Post class.

---
+++ StackOverflowPostMock/Post.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackOverflowPostMock
+{
+    class Post
+    {
+        public int VoteCount { get; } = 0;
+        public string Title { get; set; }
+        public DateTime Created { get; private set; }
+
+        public Post()
+        {
+            Created = DateTime.Now;
+        }
+    }
+}

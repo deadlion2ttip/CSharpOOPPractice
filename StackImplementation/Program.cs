bc426704f2512c95ee7fcbc92db380e496b62a6a
deadion2ttip <39771562+deadion2ttip@users.noreply.github.com>
2018-10-09 17:10:38 -0400

    Can push to and check validity of the stack.

--- StackImplementation/Program.cs
+++ StackImplementation/Program.cs
@@ -10,6 +10,16 @@ namespace StackImplementation
     {
         static void Main(string[] args)
         {
+
+            Stack stack1 = new Stack();
+
+            stack1.push(2);
+            stack1.CheckList();
+            stack1.push("cheese");
+            stack1.CheckList();
+            stack1.push(DateTime.Today);
+            stack1.CheckList();
+            stack1.push(null);
         }
     }
 }
---
+++ StackImplementation/Stack.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackImplementation
+{
+    class Stack
+    {
+        private List<Object> _store = new List<Object>();
+
+        public void push(object input)
+        {
+            if (input == null)
+            {
+                throw new InvalidOperationException("input must not be null");
+            }
+            _store.Add(input);
+        }
+
+        public void CheckList()
+        {
+            foreach (object item in _store)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}

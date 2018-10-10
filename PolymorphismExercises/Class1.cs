72d793d54c87d5f508512114d89abf040406ac1a
deadion2ttip <39771562+deadion2ttip@users.noreply.github.com>
2018-10-10 16:29:26 -0400

    Initial commit polymorphism practice, setting up a base abstract class.

---
+++ PolymorphismExercises/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismExercises
+{
+    public abstract class DbConnection
+    {
+        public string ConnectionString { get; private set; }
+        public TimeSpan Timeout { get; set; }
+
+        public DbConnection(string connection)
+        {
+            if (connection != null && !String.IsNullOrWhiteSpace(connection))
+            {
+                this.ConnectionString = connection;
+            }
+            else
+            {
+                throw new ArgumentException("Connection must be initialized with a connnection string.", "connection");
+            }
+        }
+    }
+}

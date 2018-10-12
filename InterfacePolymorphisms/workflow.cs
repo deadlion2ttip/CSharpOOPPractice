5098c08d29585ee7a5a0e8673ba9b9465515d7a6
deadion2ttip <39771562+deadion2ttip@users.noreply.github.com>
2018-10-12 14:03:44 -0400

    Action interface and workflow program created, next steps implementing the interface into several different actions.

---
+++ InterfacePolymorphisms/workflow.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacePolymorphisms
+{
+    class workflow
+    {
+        private List<IAction> _program;
+
+        public workflow(List<IAction> program)
+        {
+            if (program.Count > 0 && program != null)
+            {
+                _program = program;
+            }
+            else
+            {
+                throw new ArgumentException("Program must contain at least one action.", "program");
+            }
+        }
+
+        public void Run()
+        {
+            foreach (IAction action in _program)
+            {
+                action.Execute();
+            }
+        }
+
+    }
+}
